eee97851b87d3c2fdc0739ed256c4e527dd016e7
Hongbusi <coderhbs@gmail.com>
2022-11-08 22:36:25 +0800

    chore(playground): update

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -3,11 +3,23 @@ import { UnButton, UnIcon, UnLink } from 'unstyled-design'
 </script>
 
 <template>
-  <UnButton>Default Button</UnButton>
-  <br>
-  <UnLink target="_blank" href="https://www.hongbusi.com">
-    Link
-  </UnLink>
-  <br>
-  <UnIcon icon="i-carbon-asleep-filled" />
+  <div class="mt-10 space-y-3 text-center">
+    <div class="space-x-3">
+      <UnButton>Default Button</UnButton>
+      <UnButton icon="i-carbon-asleep-filled">
+        Icon Button
+      </UnButton>
+    </div>
+
+    <div class="space-x-3">
+      <UnLink target="_blank" href="https://www.hongbusi.com">
+        Link
+      </UnLink>
+    </div>
+
+    <div class="space-x-3">
+      <UnIcon icon="i-carbon-sun" />
+      <UnIcon icon="i-carbon-moon" />
+    </div>
+  </div>
 </template>
