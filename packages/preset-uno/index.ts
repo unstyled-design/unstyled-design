abdb589845a0aca187d51bc5a2875acab89e374f
Hongbusi <coderhbs@gmail.com>
2022-11-08 11:08:38 +0800

    chore(preset-uno): button shortcut

--- packages/preset-uno/index.ts
+++ packages/preset-uno/index.ts
@@ -3,9 +3,10 @@ import type { Preset } from 'unocss'
 export function presetUnstyled(): Preset {
   return {
     name: '@unstyled-design/preset-uno',
+
     shortcuts: [
       {
-        'un-button': 'bg-red p-2 rounded'
+        'un-button': 'px-2 py-1 text-base text-white bg-blue rounded transition-opacity hover:opacity-90'
       }
     ]
   }
