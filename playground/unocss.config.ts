8380eb27dc15b22a324acc4134da65f291e8d71e
Hongbusi <coderhbs@gmail.com>
2022-11-08 22:11:30 +0800

    chore: update lock file

--- playground/unocss.config.ts
+++ playground/unocss.config.ts
@@ -4,11 +4,11 @@ export default defineConfig({
   presets: [
     presetUno(),
     presetIcons({
-      prefix: '',
-      scale: 1.2,
       extraProperties: {
         'display': 'inline-block',
-        'vertical-align': 'middle'
+        'height': '1.2em',
+        'width': '1.2em',
+        'vertical-align': 'text-bottom'
       }
     })
   ]
