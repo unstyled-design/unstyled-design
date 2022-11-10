1022d02b31c7522defff47f0dee91db6a8f7de0c
Hongbusi <coderhbs@gmail.com>
2022-11-10 23:43:56 +0800

    docs: remove test demo

--- packages/components/link/demo/basic.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="space-x-3">
-    <UnButton>Default Button</UnButton>
-    <UnButton icon="i-carbon-data-enrichment">
-      Icon Button
-    </UnButton>
-  </div>
-</template>
