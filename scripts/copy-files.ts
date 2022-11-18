f0fb7312964d55f62b1bab6bb762dc571c856cf2
Hongbusi <coderhbs@gmail.com>
2022-11-18 11:42:09 +0800

    chore: add copy files script

---
+++ scripts/copy-files.ts
@@ -0,0 +1,13 @@
+import { copyFileSync } from 'fs'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+// relative to scripts directory
+const destinations = [
+  ['../README.md', '../packages/unstyled-design/README.md']
+]
+
+const _filename = fileURLToPath(import.meta.url)
+destinations.forEach(([src, dest]) => {
+  copyFileSync(resolve(_filename, '..', src), resolve(_filename, '..', dest))
+})
