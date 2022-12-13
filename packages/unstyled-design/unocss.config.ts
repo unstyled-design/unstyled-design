ef009980a33d93f0c8bf0db9919f3ab495d134ec
Hongbusi <coderhbs@gmail.com>
2022-12-13 17:22:20 +0800

    chore: add dep unocss-preset-hbs

--- packages/unstyled-design/unocss.config.ts
+++ packages/unstyled-design/unocss.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig, presetUno } from 'unocss'
+import { presetHbs } from 'unocss-preset-hbs'
 import { presetUnstyled } from '@unstyled-design/preset-uno'
 
 export default defineConfig({
   presets: [
     presetUno(),
+    presetHbs(),
     presetUnstyled()
   ]
 })
